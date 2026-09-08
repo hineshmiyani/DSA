f8d112aba5fbfb4ad1a0535f678e47c4a24a09a2
Hinesh Miyani <48402698+hineshmiyani@users.noreply.github.com>
2026-09-08 17:43:14 +0530

    Problem: 0238-product-of-array-except-self, Time: 3 ms (91.49%), Space: 67.5 MB (94.45%)

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -4,24 +4,23 @@
  */
 var productExceptSelf = function(nums) {
     
+    const result = new Array(nums.length).fill(1); // [1, 1, 2, 6]
+    // const result = new Array(nums.length).fill(1); // [24, 12, 8, 6]
+   
 
-    const prefixSum = new Array(nums.length).fill(1); // [1, 1, 2, 6]
-    const suffixSum = new Array(nums.length).fill(1); // [24, 12, 4, 1]
     // Prefix Sum
+
     for (let i = 1; i < nums.length; i++) {
-        prefixSum[i] = prefixSum[i - 1] * nums[i - 1]; 
+        result[i] = result[i - 1] * nums[i - 1]; 
     }
 
     // Suffix Sum
+
+    let suffixSum = 1; // 1, 4, 12, 24
     for (let i = nums.length - 1 - 1; i >= 0; i--) {
-        suffixSum[i] = suffixSum[i + 1] * nums[i + 1];
-
+        result[i] = result[i] * suffixSum * nums[i + 1];  // 1 * 12 * 2
+        suffixSum = suffixSum * nums[i + 1]; // 24
     }
 
-    // Multipliaction
-    for (let i = 0; i < nums.length; i++) {
-        prefixSum[i] = prefixSum[i] * suffixSum[i];
-    }
-
-    return prefixSum;
+    return result;
 };
