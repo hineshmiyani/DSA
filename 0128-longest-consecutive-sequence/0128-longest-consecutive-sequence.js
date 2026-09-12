56f1022814a0c74260bd7dbb7c485bd398b919df
Hinesh Miyani <48402698+hineshmiyani@users.noreply.github.com>
2026-09-12 22:20:01 +0530

    Problem: 0128-longest-consecutive-sequence, Time: 53 ms (15.04%), Space: 81.3 MB (22.85%)

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -2,26 +2,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
+var longestConsecutive = function (nums) {
     if (nums.length === 0) return 0;
 
 
-    const sortedNums = [...new Set(nums)].sort((a, b) => a - b); // [1,2,3,4,100,200]
+    const numsSet = new Set(nums);
 
+    let maxSequence = 1;
 
-    let maxSequence = 1; // 4
-    let currentSequence = 1; // 1
+    for (let currentNum of numsSet) {
 
-    for (let i = 1; i < sortedNums.length; i++) {
+        let currentSequence = 1;
 
-        if ((sortedNums[i - 1] + 1) === sortedNums[i]) { // 101===200
-            currentSequence = currentSequence + 1; 
-        } else {
-            currentSequence = 1;
+        if (!numsSet.has(currentNum - 1)) {
+
+            numsSet.delete()
+            while (numsSet.has(currentNum + 1)) {
+                numsSet.delete(currentNum + 1);
+                
+                currentSequence = currentSequence + 1;
+                currentNum = currentNum + 1;
+            }
         }
 
         maxSequence = Math.max(maxSequence, currentSequence);
-
     }
 
     return maxSequence;
